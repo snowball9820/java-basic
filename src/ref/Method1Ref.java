711247da0913251de6a18668eee0f946d1d2a732
LAPTOP-OGNPS07V\moons <snowball9820@gmail.com>
2024-01-16 07:14:10 +0900

    Reference Type_Method3

--- src/ref/Method1Ref.java
+++ src/ref/Method1Ref.java
@@ -16,14 +16,16 @@ public class Method1Ref {
 
     //대입하는 부분도 메서드로 만들기
     //참조형은 메서드를 호출할 때 참조값을 저장함, 따라서 메서드 내부에서 전달된 참조값을 통해 객체의 값을 변경하거나 값을 읽어서 사용 가능
+    //initStudent는 전달한 학생 객체의 필드 값을 설정함
     static void initStudent(Student student, String name, int age, int grade) {
-        student.name=name;
-        student.age=age;
-        student.grade = grade;
+        student.name=name; //참조값을 통해서 student.name에 접근하는데 매개변수 name에 들어온 값을 넣어주게 된다.
+        student.age=age; //참조값을 통해서 student.age에 접근하는데 매개변수 age에 들어온 값을 넣어주게 된다.
+        student.grade = grade; //참조값을 통해서 student.grade에 접근하는데 매개변수 grade에 들어온 값을 넣어주게 된다.
     }
 
 
     //이제 student1, student2 인스턴스에 저장된 값들을 대입하고 결과가 출력되도록 해줌
+    //printStudent는 전달한 학생 객체의 필드 값을 읽어서 출력함
     static void printStudent(Student student) {
         System.out.println("이름 : "+student.name+", 나이 : "+student.age+", 성적 : "+student.grade);
     }
