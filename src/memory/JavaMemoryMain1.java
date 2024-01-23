ac64976c27e7a5befe2c65179d45fc062ccec203
LAPTOP-OGNPS07V\moons <snowball9820@gmail.com>
2024-01-23 21:39:44 +0900

    memory/method stack

---
+++ src/memory/JavaMemoryMain1.java
@@ -0,0 +1,23 @@
+package memory;
+
+public class JavaMemoryMain1 {
+
+    public static void main(String[] args) {
+        System.out.println("main start");
+        method1(10);
+        System.out.println("main end");
+
+    }
+
+    static void method1(int m1) {
+        System.out.println("method1 start");
+
+        System.out.println("method1 end");
+
+
+    }
+
+    static void method2(int m2) {
+
+    }
+}
