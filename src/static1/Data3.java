5d78b4ca8952ea53bc12573f85bf6139b1fc7905
LAPTOP-OGNPS07V\moons <snowball9820@gmail.com>
2024-01-26 01:26:23 +0900

    static/static1_DataCountMain3,Data3 Ref2

--- src/static1/Data3.java
+++ src/static1/Data3.java
@@ -8,6 +8,7 @@ public class Data3 {
     public Data3(String name) {
         this.name = name;
         count++; //붕어빵 틀에 접근을 해서 바로 증가시키는 것과 같음 메서드 영역이니까
+        //Data3의 생성자와 같이 자신의 클래스에 있는 정적 변수라면 클래스명 생략 가능 Data3.count->count
     }
 }
 //멤버 변수에 static을 붙이게 되면 static 변수, 정적 변수, 클래스 변수라고 함
--- src/static1/DataCountMain3.java
+++ src/static1/DataCountMain3.java
@@ -5,13 +5,14 @@ public class DataCountMain3 {
 
         Data3 data1 = new Data3("A");
         System.out.println("A count = " + Data3.count); //data1.count가 아님 정적 변수에 접근하는 방법은 다름
+        //static에 있는 count1 증가
 
         Data3 data2 = new Data3("B");
         System.out.println("B count = " + Data3.count);
-
+        //static에 있는 count1 증가
         Data3 data3 = new Data3("C");
         System.out.println("C count = " + Data3.count);
-
+        //static에 있는 count1 증가
     }
 }
 //정적 변수에 접근하기 위해 Data3.count와 같이 클래스명에 . 을 사용함, 클래스에 직접 접근하는 것 처럼 느껴짐
@@ -26,3 +27,7 @@ public class DataCountMain3 {
 //생성자에는 count++코드가 있음 count는 static이 붙은 정적 변수임, 정적 변수는 메서드 영역에서 관리함
 //메서드 영역에 있는 count값이 하나 증가 함
 //붕어빵 틀에서 관리하는 변수라고 보면 됩니당
+
+//정리
+//static변수는 쉽게 이야기해서 붕어빵 틀이 특별히 관리하는 변수! 붕어빵 틀은 1개니까 클래스 변수도 하나만 존재
+//인스턴스는 붕어빵 인스턴스 수만큼 변수 존재...
