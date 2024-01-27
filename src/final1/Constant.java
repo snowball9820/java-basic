644428c4e9187e99838eb93494d39773701cceca
LAPTOP-OGNPS07V\moons <snowball9820@gmail.com>
2024-01-28 04:33:54 +0900

    final/ConstantEx2

--- src/final1/Constant.java
+++ src/final1/Constant.java
@@ -17,7 +17,7 @@ public class Constant {
     public static final int MINUTE_IN_HOUR = 60;
     public static final int SECONDS_IN_MINUTE = 60;
     //애플리케이션 설정 상수
-    public static final int MAX_USERS = 1000;
+    public static final int MAX_USERS = 1000; //중앙에서 한번에 관리 가능 여기서 변경하면 됨 ConstantMain1/2
 
 }
 //애플리케이션 안에는 다양한 상수 존재, 수학/시간 등 실생활에서 사용하는 상수부터 애플리케이션 설정을 위한 상수 존재
--- src/final1/ConstantMain2.java
+++ src/final1/ConstantMain2.java
@@ -1,8 +1,8 @@
 package final1;
 
-public class ConstantMain1 {
+public class ConstantMain2 {
     public static void main(String[] args) {
-        System.out.println("프로그램 최대 참여자 수 = "+1000); //2000으로 고치면?
+        System.out.println("프로그램 최대 참여자 수 = "+Constant.MAX_USERS); //2000으로 고치면? 그냥 Constant로 가서 2000으로 하나만 수정
         int currentUserCount=999;
         process(currentUserCount++);
         process(currentUserCount++);
@@ -12,7 +12,7 @@ public class ConstantMain1 {
 
     private static void process(int currentUserCount) {
         System.out.println("참여자 수 = "+currentUserCount);
-        if (currentUserCount > 1000) { //1000명 초과하면 대기자로등록 //여기도 2000으로 고쳐야됨
+        if (currentUserCount > Constant.MAX_USERS) { //Constant.MAX_USERS 변수명만 봐도 어떤 수를 의미하는지 바로 파악
             System.out.println("대기자 등록");
         } else {
             System.out.println("게임에 참여가능");
