90e5679d9403adcbe0f6a85039631d887ab6b9c5
LAPTOP-OGNPS07V\moons <snowball9820@gmail.com>
2024-01-27 23:48:49 +0900

    static/static ex MathArrayUtilsMain

---
+++ src/static2/ex/MathArrayUtilsMain.java
@@ -0,0 +1,14 @@
+package static2.ex;
+
+public class MathArrayUtilsMain {
+    //배열용 수학 유틸리티 클래스를 만들자
+    //sum(int[] array)  배열의 모든 요소를 더해 합계 반환
+    //average(int[] array) 배열의 모든 요소의 평균값을 계산
+    //min(int[] array) 배열에서 최소값을 찾음
+    //max(int[] array) 배열에서 최대값을 찾음
+
+
+    public static void main(String[] args) {
+
+    }
+}
