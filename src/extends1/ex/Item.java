de9d8b601b30f6ca4fe42b0ea6bd5d7cb6fec72b
LAPTOP-OGNPS07V\moons <snowball9820@gmail.com>
2024-01-31 23:11:22 +0900

    extends/ex Shop_2 Book, Album, Movie

---
+++ src/extends1/ex/Item.java
@@ -0,0 +1,23 @@
+package extends1.ex;
+
+public class Item {
+    private String name;
+    private int price;
+
+    //생성자
+    public Item(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void print() {
+
+        System.out.println("이름 : "+name+", 가격 : "+price);
+    }
+}
+
+
