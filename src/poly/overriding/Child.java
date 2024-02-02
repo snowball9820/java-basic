251a13d9d4edb86231e5bf100d599f4ddeb35efb
LAPTOP-OGNPS07V\moons <snowball9820@gmail.com>
2024-02-03 02:00:41 +0900

    poly/poly method overriding3

--- src/poly/overriding/Child.java
+++ src/poly/overriding/Child.java
@@ -4,6 +4,7 @@ public class Child extends Parent {
     public String value = "child";
 
     //ctrl+o -> overriding 자동완성
+    //지금 Parent의 method를 오버라이딩했기 때문에 우선권을 가진 상태
     @Override
     public void method() {
         System.out.println("Child.method");//재정의
--- src/poly/overriding/OverridingMain.java
+++ src/poly/overriding/OverridingMain.java
@@ -19,9 +19,22 @@ public class OverridingMain {
         //부모 변수가 자식 인스턴스 참조(다형적 참조)
         Parent poly = new Child(); //인스턴스는 자식, 참조는 부모
         System.out.println("Parent->Child");
-        System.out.println("value = "+poly.value); //변수는 오버라이딩 불가
+        System.out.println("value = "+poly.value); //변수는 오버라이딩 불가 value = parent
         poly.method();//메서드는 오버라이딩 가능 왜 Child.method???
 
-
+        //poly변수는 Parent타입 따라서 poly.value, poly.method()를 찾아 호출하면 인스턴스의 Parent타입에서 기능을 찾음
+        //poly.value:Parent타입에 있는 value값을 읽음
+        //poly.method():Parent타입에 있는 method()를 실행하려고 함 but
+        //하위타입인 Child.method()가 오버라이딩 되어 있음!!!!
+        //***오버라이딩 된 메서드는 항상* 언제나* 우선권을 가짐 따라서 Parent에서 내려와 Child.method()가 실행됨
+        //오버라이딩은 부모타입에서 정의한 기능을 자식 타입에서 재정의 하는 것임
+        //만약 자식에서도 오버라이딩, 손자에서도 같은 메서드를 오버라이딩하면 **손자의 오버라이딩 메서드가 최우선!
+        //더 하위 자식의 오버라이딩 된 메서드가 절대적 우선권을 쥠
+        //마지막에 오버라이딩 된 곳까지 내려가서 method를 찾음
     }
 }
+
+//다형적 참조 & 메서드 오버라이딩
+//다형적 참조: 하나의 변수 타입으로 다양한 자식 인스턴스를 참조할 수 있는 기능
+//메서드 오버라이딩: 기존 기능을 하위 타입에서 새로운 기능으로 재정의
+
