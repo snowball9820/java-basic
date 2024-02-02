d45203255b3fc4e20735fd3841d19a9529f793f0
LAPTOP-OGNPS07V\moons <snowball9820@gmail.com>
2024-02-03 01:44:56 +0900

    poly/poly overriding2

--- src/poly/overriding/OverridingMain.java
+++ src/poly/overriding/OverridingMain.java
@@ -7,5 +7,21 @@ public class OverridingMain {
         System.out.println("Child->Child");
         System.out.println("value = " + child.value);
         child.method();
+        //child 변수는 Child타입 따라서 child.value, child.method() 모두 Child타입에서 기능을 찾고 실행
+
+        //부모변수가 부모인스턴스 참조
+        Parent parent = new Parent();
+        System.out.println("Parent->Parent");
+        System.out.println("value = " + parent.value);
+        parent.method();
+        //parend 변수는 Parent타입 따라서 parent.value, parent.method()를 호출하면 모두 Parent타입에서 기능을 찾고 실행
+
+        //부모 변수가 자식 인스턴스 참조(다형적 참조)
+        Parent poly = new Child(); //인스턴스는 자식, 참조는 부모
+        System.out.println("Parent->Child");
+        System.out.println("value = "+poly.value); //변수는 오버라이딩 불가
+        poly.method();//메서드는 오버라이딩 가능 왜 Child.method???
+
+
     }
 }
