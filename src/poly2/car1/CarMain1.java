88422a8702c199d150ce7fb2d6f3e5f1d38a0426
LAPTOP-OGNPS07V\moons <snowball9820@gmail.com>
2024-02-17 21:05:17 +0900

    poly/polymorphism ex2_Ref

--- src/poly2/car1/CarMain1.java
+++ src/poly2/car1/CarMain1.java
@@ -7,5 +7,11 @@ public class CarMain1 {
         //차량 선택(k3)
         K3Car k3Car = new K3Car();
         driver.setCar(k3Car);
+        driver.driver();
+
+        //차량 변경(k->model3)
+        Model3Car model3Car = new Model3Car();
+        driver.setCar(model3Car);
+        driver.driver();
     }
 }
