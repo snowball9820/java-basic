59eb7cf4e1c4bfb31e7a4865ea2367cb9c27c10e
LAPTOP-OGNPS07V\moons <snowball9820@gmail.com>
2024-02-17 20:41:33 +0900

    poly/polymorphism ex1

--- src/poly2/car0/CarMain0.java
+++ src/poly2/car0/CarMain0.java
@@ -4,9 +4,16 @@ public class CarMain0 {
     public static void main(String[] args) {
         Driver driver = new Driver();
         K3Car k3Car = new K3Car();
-
         driver.setK3car(k3Car); //이 시점에 k3 인스턴스를 가지게 됨
         driver.drive();
+
+        //추가
+        Model3Car model3Car = new Model3Car();
+        driver.setK3car(null);
+        driver.setModel3Car(model3Car);
+
+        driver.drive();
+
 
     }
 }
