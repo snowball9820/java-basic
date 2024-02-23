67c752f3e66fe2540d91b9a811e35bfa6dd10470
LAPTOP-OGNPS07V\moons <snowball9820@gmail.com>
2024-02-23 21:03:35 +0900

    poly/polymorphism ex pay1_Ref

--- src/poly/ex/pay1/PayService.java
+++ src/poly/ex/pay1/PayService.java
@@ -3,21 +3,22 @@ package poly.ex.pay1;
 public class PayService {
     public void processPay(String option, int amount) {
 
-        boolean result=false;
+        boolean result = false;
         System.out.println("결제를 시작합니다: option= " + option + " , amount= " + amount);
-
-        //결제 수단 선택
-        Pay pay;
-        if (option.equals("kakao")) {
-            pay = new KakaoPay(); //객체를 계속 만들고 있음 추가할 때마다 이걸 인터페이스로 빼야지
-        } else if (option.equals("naver")) {
-            pay = new NaverPay();
-        } else {
-            pay = null;
-            System.out.println("결제 수단이 없습니다.");
-        }
-        //실제결제
-        //nullPointerException 처리
+        //1.결제 수단 선택 -> 메서드로 빼기 전
+//        Pay pay;
+//        if (option.equals("kakao")) {
+//            pay = new KakaoPay(); //객체를 계속 만들고 있음 추가할 때마다 이걸 인터페이스로 빼야지
+//        } else if (option.equals("naver")) {
+//            pay = new NaverPay();
+//        } else {
+//            pay = null;
+//            System.out.println("결제 수단이 없습니다.");
+//        }
+        //1.결제 수단 선택 -> 메서드로 뽑은 결과물
+        Pay pay = findPay(option);
+        //2.실제결제
+        //NullPointerException 처리
         if (pay != null) {
             result = pay.pay(amount);
         }
@@ -28,5 +29,17 @@ public class PayService {
             System.out.println("결제가 실패했습니다.");
         }
 
+    }
+
+    public Pay findPay(String option) {
+        Pay pay;
+        if (option.equals("kakao")) {
+            return new KakaoPay();
+        } else if (option.equals("naver")) {
+            return new NaverPay();
+        } else {
+            System.out.println("결제 수단이 없습니다.");
+            return null;
+        }
     }
 }
